2aeb45930a1a4db312fba7c1ff72d69599176f59
hatappi <3453977+hatappi@users.noreply.github.com>
2022-05-04 21:58:08 +0900

    Add ReceiverConfig

---
+++ config/receiver_test.go
@@ -0,0 +1,111 @@
+//go:build proxytest
+
+package config
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestLoadReceiverConfig(t *testing.T) {
+	testCases := map[string]struct {
+		inputData []byte
+
+		expectedConfig *ReceiverConfig
+		wantErr        bool
+	}{
+		"valid inputData": {
+			inputData: []byte(`
+				{
+					"cloud_watch_region": "ap-northeast-1",
+					"cloud_watch_cluster_name": "cloudwatch_cluster",
+					"aws_access_key_id": "foo",
+					"aws_secret_access_key": "bar",
+					"http_request_timeout_millisecond": 100
+				}
+			`),
+			expectedConfig: &ReceiverConfig{
+				CloudWatchRegion:              "ap-northeast-1",
+				CloudWatchClusterName:         "cloudwatch_cluster",
+				AWSAccessKeyID:                "foo",
+				AWSSecretAccessKey:            "bar",
+				HTTPRequestTimeoutMillisecond: 100,
+			},
+			wantErr: false,
+		},
+		"nil inputData": {
+			inputData:      nil,
+			expectedConfig: nil,
+			wantErr:        true,
+		},
+		"cloud_watch_region is empty": {
+			inputData: []byte(`
+				{
+					"cloud_watch_cluster_name": "cloudwatch_cluster",
+					"aws_access_key_id": "foo",
+					"aws_secret_access_key": "bar"
+				}
+			`),
+			expectedConfig: nil,
+			wantErr:        true,
+		},
+		"cloud_watch_cluster_name is empty": {
+			inputData: []byte(`
+				{
+					"cloud_watch_region": "ap-northeast-1",
+					"aws_access_key_id": "foo",
+					"aws_secret_access_key": "bar"
+				}
+			`),
+			expectedConfig: nil,
+			wantErr:        true,
+		},
+		"aws_access_key_id is empty": {
+			inputData: []byte(`
+				{
+					"cloud_watch_region": "ap-northeast-1",
+					"cloud_watch_cluster_name": "cloudwatch_cluster",
+					"aws_secret_access_key": "bar"
+				}
+			`),
+			expectedConfig: nil,
+			wantErr:        true,
+		},
+		"aws_secret_access_key is empty": {
+			inputData: []byte(`
+				{
+					"cloud_watch_region": "ap-northeast-1",
+					"cloud_watch_cluster_name": "cloudwatch_cluster",
+					"aws_access_key_id": "foo"
+				}
+			`),
+			expectedConfig: nil,
+			wantErr:        true,
+		},
+		"empty inputData": {
+			inputData:      []byte(`{}`),
+			expectedConfig: nil,
+			wantErr:        true,
+		},
+		"invalid inputData": {
+			inputData:      []byte(`foo`),
+			expectedConfig: nil,
+			wantErr:        true,
+		},
+	}
+
+	for name, tc := range testCases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			c, err := LoadReceiverConfig(tc.inputData)
+			if tc.wantErr != (err != nil) {
+				t.Errorf("err: %v", err)
+			}
+
+			if diff := cmp.Diff(c, tc.expectedConfig); diff != "" {
+				t.Errorf("(-got, +want)\n%s", diff)
+			}
+		})
+	}
+}
